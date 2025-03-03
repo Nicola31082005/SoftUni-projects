8cf30de87fdf3c5bd34fb29091ac01d8231243cd
Nicola31082005 <nikule512@gmail.com>
2025-03-03 15:32:16 +0200

    Fetch users from server and add to UserListItem

---
+++ react-user-list/client/src/services/userService.js
@@ -0,0 +1,11 @@
+const URL = 'http://localhost:3030/jsonstore/users'
+
+export default {
+    async getAll() {
+        const response = await fetch(URL);
+        const result = await response.json();
+        const users = Object.values(result);
+
+        return users
+    }
+}
