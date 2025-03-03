88a4c348fe3647bd29fc46481516937638a33893
Nicola31082005 <nikule512@gmail.com>
2025-03-03 14:44:00 +0200

    Ad UserList component

--- react-user-list/client/src/App.jsx
+++ react-user-list/client/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import UserList from './components/UserList'
 
 function App() {
 
   return (
     <>
       <Header />
-
-
+      <main className="main">
+        <UserList />
+      </main>
       <Footer />
     </>
   )
