4ee6f0b0a62a94eef4ffb5be1d264f0d168f6094
Nicola31082005 <nikule512@gmail.com>
2025-03-03 17:20:58 +0200

    Add userCreate working functionality

--- react-user-list/client/src/components/UserList.jsx
+++ react-user-list/client/src/components/UserList.jsx
@@ -2,16 +2,43 @@ import { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import UserListItem from "./UserListItem";
 import userService from "../services/userService";
+import UserCreate from "./UserCreate";
 
 export default function UserList() {
 
     const [users, setUsers] = useState([])
+    const [showCreate, setShowCreate] = useState(null)
 
     useEffect(() => {
         userService.getAll()
             .then((result) => setUsers(result))
-
     }, [])
+
+    const createUserClickHandler = () => {
+        setShowCreate(true)
+    }
+
+    const closeUserClickHandler = () => {
+        setShowCreate(null)
+    }
+
+    const saveUserClickHandler = async (e) => {
+        // Stop default refresh
+        e.preventDefault();
+
+        const formData = new FormData(e.target.parentElement.parentElement);
+        const userData = Object.fromEntries(formData.entries());
+
+        // Create new user on the server
+        const newUser = await userService.addUser(userData);
+
+        // Update local state
+        setUsers(users => [...users, newUser]);
+
+        // Close the modal
+        setShowCreate(false);
+
+    }
 
 
 
@@ -20,9 +47,16 @@ export default function UserList() {
 
             <SearchBar />
 
+            {showCreate && (
+                <UserCreate
+                    onClose={closeUserClickHandler}
+                    onSave={saveUserClickHandler}
+                />
+            )}
+
             <div className="table-wrapper">
 
-                <div id="overlap components">
+                <div id="overlays">
                     {/* <!-- Overlap components  -->
 
             <!-- <div className="loading-shade"> -->
@@ -158,57 +192,9 @@ export default function UserList() {
             </div>
 
             {/* <!-- New user button  --> */}
-            <button className="btn-add btn">Add new user</button>
+            <button onClick={createUserClickHandler} className="btn-add btn">Add new user</button>
 
-            {/* <!-- Pagination component  --> */}
-            {/* <div className="pagination position">
-            <div className="limits">
-              <span>Items per page:</span>
-              <select name="limit" className="limit" value="5">
-                <option value="5">5</option>
-                <option value="5">10</option>
-                <option value="5">15</option>
-                <option value="5">20</option>
-              </select>
-            </div>
-            <p className="pages">1 - 1 of 1</p>
-            <div className="actions">
-              <button className="btn" title="First Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angles-left"
-                  className="svg-inline--fa fa-angles-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path fill="currentColor"
-                    d="M77.25 256l137.4-137.4c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0l-160 160c-12.5 12.5-12.5 32.75 0 45.25l160 160C175.6 444.9 183.8 448 192 448s16.38-3.125 22.62-9.375c12.5-12.5 12.5-32.75 0-45.25L77.25 256zM269.3 256l137.4-137.4c12.5-12.5 12.5-32.75 0-45.25s-32.75-12.5-45.25 0l-160 160c-12.5 12.5-12.5 32.75 0 45.25l160 160C367.6 444.9 375.8 448 384 448s16.38-3.125 22.62-9.375c12.5-12.5 12.5-32.75 0-45.25L269.3 256z">
-                  </path>
-                </svg>
-              </button>
 
-              <button className="btn" title="Previous Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left"
-                  className="svg-inline--fa fa-angle-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
-                  <path fill="currentColor"
-                    d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z">
-                  </path>
-                </svg>
-              </button>
-              <button className="btn" title="Next Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right"
-                  className="svg-inline--fa fa-angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
-                  <path fill="currentColor"
-                    d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z">
-                  </path>
-                </svg>
-              </button>
-
-              <button className="btn" title="Last Page">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angles-right"
-                  className="svg-inline--fa fa-angles-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path fill="currentColor"
-                    d="M246.6 233.4l-160-160c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L178.8 256l-137.4 137.4c-12.5 12.5-12.5 32.75 0 45.25C47.63 444.9 55.81 448 64 448s16.38-3.125 22.62-9.375l160-160C259.1 266.1 259.1 245.9 246.6 233.4zM438.6 233.4l-160-160c-12.5-12.5-32.75-12.5-45.25 0s-12.5 32.75 0 45.25L370.8 256l-137.4 137.4c-12.5 12.5-12.5 32.75 0 45.25C239.6 444.9 247.8 448 256 448s16.38-3.125 22.62-9.375l160-160C451.1 266.1 451.1 245.9 438.6 233.4z">
-                  </path>
-                </svg>
-              </button>
-            </div>
-          </div> */}
         </section>
     )
 }
--- react-user-list/client/src/services/userService.js
+++ react-user-list/client/src/services/userService.js
@@ -7,5 +7,38 @@ export default {
         const users = Object.values(result);
 
         return users
+    },
+    async getOne(userId) {
+        const response = await fetch(`${URL}/${userId}`);
+        const user = await response.json();
+
+        return user;
+    },
+    async addUser(data) {
+        const postData = transformUserData(data)
+
+        const respone = await fetch(URL, {
+            method: "POST",
+            headers: {
+             "Content-Type": "application/json",
+            },
+            body: JSON.stringify(postData)
+        })
+
+        const result = respone.json()
+
+        return result;
     }
+}
+
+
+function transformUserData(data) {
+
+    const { country, city, street, streetNumber, ...transformedData } = data;
+
+    transformedData.address = { country, city, street, streetNumber };
+    transformedData.createdAt = new Date().toISOString();
+    transformedData.updatedAt = new Date().toISOString();
+
+    return transformedData;
 }
